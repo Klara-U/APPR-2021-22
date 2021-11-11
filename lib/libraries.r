49438b7d8a4d63c33eaaea8531c62e44de509c49
Klara Ursic <ursic.klara@gmail.com>
2021-11-11 09:07:01 +0100

    test

--- lib/libraries.r
+++ lib/libraries.r
@@ -6,8 +6,12 @@ library(tmap)
 library(shiny)
 library(readr)
 library(dplyr)
+library(tabit)
+
+
+
 
 options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
-source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+source("lib/uvozi.zemljevid.r")
